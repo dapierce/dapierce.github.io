6a43e46a5e0cca0b0a77f0ba26833e3c77807662
David Pierce <davidaaronpierce@gmail.com>
2023-04-18 22:26:19 +0000

    Design updates

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,12 +1,12 @@
 module.exports = {
-	title: "David🥧",
-	url: "https://davidaaronpierce.com/",
-	language: "en",
-	description:
-		"Personal website of David Aaron Pierce, artist, occasional coder and video game liker.",
-	author: {
-		name: "David Aaron Pierce",
-		email: "davidaaronpierce@gmail.com",
-		url: "https://davidaaronpierce.com/about-me/",
-	},
+  title: "David🥧",
+  url: "https://davidaaronpierce.com/",
+  language: "en",
+  description:
+    "Personal website of David Aaron Pierce, artist, occasional coder and video game liker.",
+  author: {
+    name: "David Aaron Pierce",
+    email: "davidaaronpierce@gmail.com",
+    url: "https://davidaaronpierce.com/about-me/",
+  },
 };
